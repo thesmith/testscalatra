5d6771994a40f70773a6cb496b1034fac9a73c0f
Ben Smith <ben@thesmith.co.uk>
2011-03-13 23:04:49 +0000

    removed unnecessary lines

--- src/main/scala/scalate/Boot.scala
+++ src/main/scala/scalate/Boot.scala
@@ -5,13 +5,10 @@ import org.fusesource.scalate.TemplateEngine
 import java.io.File
 
 class Boot(engine: TemplateEngine) extends Logging {
-  val templatePath = "/WEB-INF/scalate/templates/hello-scalate.scaml"
 
   def run: Unit = {
     engine.workingDirectory = new File("/tmp")
     engine.allowCaching = true
     engine.allowReload = false
-
-    engine.layout(templatePath)
   }
 }
